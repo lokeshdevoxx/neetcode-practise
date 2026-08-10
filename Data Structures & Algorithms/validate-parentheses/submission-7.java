298f5bb50ef2870b87d4daae8c078516c1906e6e
lokeshdevoxx <lokesh.devoxx@gmail.com>
2026-08-10 18:02:00 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        if(s.length()==0) return true;
+        if(s.length()==1) return false;
+        
+
+        boolean flag=true;
+        char[] charArr = s.toCharArray();
+        Stack<Character> st = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        for(char ch: charArr) {
+
+            if(map.containsKey(ch)) {
+                if(!st.isEmpty() && st.peek()==map.get(ch)){
+                    st.pop();
+                } else {
+                    return !flag;
+                }
+            } else {
+                st.push(ch);
+            }
+        }
+        return st.isEmpty();
+
+            // if(ch=='(' || ch=='[' || ch=='{') {
+            //     st.push(ch);
+            // } else {
+
+            //     if(!st.isEmpty()) {
+            //         if(ch==')' && st.peek()=='(') {
+            //             st.pop();
+            //         } else if(ch==']' && st.peek()=='[') {
+            //             st.pop();
+            //         } else if(ch=='}' && st.peek()=='{') {
+            //             st.pop();
+            //         } else {
+            //             flag=false;
+            //         }
+            //     } else {
+            //         flag=false;
+            //     }
+           // }
+        //}
+
+        // if(!st.isEmpty()) {
+        //     flag=false;
+        // }
+        // return flag;
+
+    }
+}
